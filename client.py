f843ff780ebcff656b5749aedc4253942a3447ee
DocBlack89 <antoinegibier10@hotmail.fr>
2018-03-16 08:50:50 +0100

    correction

--- client.py
+++ client.py
@@ -4,4 +4,7 @@ import config
 import ecc
 
 curve = ecc.Curve(config.A, config.B, config.N)
+isOn = ecc.Curve.isOn(config.P)
+add = ecc.Curve.add(config.P, config.Q)
+mul = ecc.Curve.mul(config.N, config.P)
 print(curve)
--- config.py
+++ config.py
@@ -4,4 +4,5 @@ A = 10
 B = 15
 N = 20
 P = ecc.Point(7, 5)
+Q = ecc.Point(8, 3)
 
