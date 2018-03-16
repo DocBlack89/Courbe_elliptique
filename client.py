23000fbffa6fc4d1e1fa4b9aaa2b15f893ee920c
DocBlack89 <antoinegibier10@hotmail.fr>
2018-03-16 08:52:34 +0100

    test

--- client.py
+++ client.py
@@ -4,7 +4,10 @@ import config
 import ecc
 
 curve = ecc.Curve(config.A, config.B, config.N)
-isOn = ecc.Curve.isOn(config.P)
-add = ecc.Curve.add(config.P, config.Q)
-mul = ecc.Curve.mul(config.N, config.P)
+isOn = ecc.Curve.isOn(curve, config.P)
+add = ecc.Curve.add(curve, config.P, config.Q)
+mul = ecc.Curve.mul(curve, config.N, config.P)
 print(curve)
+print(isOn)
+print(add)
+print(mul)
