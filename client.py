42d2c7e26a631101cc19f977b0080f4efef4c5dc
DocBlack89 <antoinegibier10@hotmail.fr>
2018-03-19 08:59:18 +0100

    Séance 19/03

--- client.py
+++ client.py
@@ -3,14 +3,84 @@
 import config
 import ecc
 
-curve = ecc.Curve(config.A, config.B, config.N)
-isOnP = ecc.Curve.isOn(curve, config.P)
-isOnQ = ecc.Curve.isOn(curve, config.P)
-add = ecc.Curve.add(curve, config.P, config.Q)
-mul = ecc.Curve.mul(curve, config.n, config.Q)
 
-print(curve)
-print(add)
-print(mul)
-print(isOnP)
-print(isOnQ)
+choix = config.menu()
+
+def creation_courbe():
+    print("Valeur de A")
+    A = int(input())
+    print("Valeur de B")
+    B = int(input())
+    print("Ordre de la courbe")
+    N = int(input())
+    curve = ecc.Curve(A, B, N)
+    return curve
+
+def multiplication_point():
+    curve = creation_courbe()
+    print("Valeur du point")
+    P = int(input())
+    print("Nombre de multiplication")
+    n = int(input())
+    mul = ecc.Curve.mul(curve, n, P)
+    print(mul)
+
+def doublement_point():
+    curve = creation_courbe()
+    print("Valeur du point")
+    P = int(input())
+    dbl = ecc.Curve.mul(curve, 2, P)
+    print(dbl)
+
+def verif_presence():
+    curve = creation_courbe()
+    print("Valeur du point")
+    P = int(input())
+    isOn = ecc.Curve.isOn(curve, P)
+    print(isOn)
+
+def addition_points():
+    curve = creation_courbe()
+    print("Valeur du point P")
+    P = int(input())
+    print("Valeur du point Q")
+    Q = int(input())
+    add = ecc.Curve.add(curve, P, Q)
+    print(add)
+
+def inverse_points():
+    print("Valeur de l'ordre de la courbe")
+    N = int(input())
+    print("Valeur du point P")
+    P = int(input())
+    inv = ecc.inv(N, P)
+    print(inv)
+
+def tout():
+    print("Valeur du point A")
+    A = int(input())
+    print("Valeur du point B")
+    B = int(input())
+    print("Valeur du point P")
+    P = int(input())
+    print("Valeur du point Q")
+    Q = int(input())
+    print("Nombre de multiplication")
+    n = int(input())
+    print("Valeur de l'ordre de la courbe")
+    N = int(input())
+    curve = ecc.Curve(A, B, N)
+    isOnP = ecc.Curve.isOn(curve, config.P)
+    isOnQ = ecc.Curve.isOn(curve, config.P)
+    add = ecc.Curve.add(curve, config.P, config.Q)
+    mul = ecc.Curve.mul(curve, config.n, config.Q)
+    dbl = ecc.Curve.mul(curve, 2, config.Q)
+    inv = ecc.inv(config.N, config.P)
+
+    print(curve)
+    print(add)
+    print(mul)
+    print(isOnP)
+    print(isOnQ)
+    print(dbl)
+    print(inv)
--- config.py
+++ config.py
@@ -1,6 +1,23 @@
 import ecc
 
 
+
+
+def menu():
+    print("######################")
+    print("#  Ceci est un menu  #")
+    print("######################")
+    print("1- Création de la courbe")
+    print("2- Multiplication de deux points")
+    print("3- Doublement d'un point")
+    print("4- Vérification de la présence du point sur la courbe")
+    print("5- Addition de deux points")
+    print("6- Calcul de l'inverse")
+    print("7- Tout")
+    choix = int(input("Votre choix"))
+    return choix
+
+
 ###### Création de la courbe elliptiques #####
 #A = 367894248157291519724356994675367555090991868479757
 #B = 650136343866907590034711240239825334223238899223249
@@ -14,9 +31,11 @@ N = 17
 ##### n pour la multiplication ####
 n = 2
 
-
 ##### Point de la courbe elliptique #####
 #P = ecc.Point(11370152050562514760725829841636301722527983698782725, 7252135185890240681446231840366738239134349061512895)
 #Q = ecc.Point(11370152050562514760725829841636301722527983698782725, 7252135185890240681446231840366738239134349061512895)
 P = ecc.Point(6, 5)
 Q = ecc.Point(2, 0)
+
+
+menu()
